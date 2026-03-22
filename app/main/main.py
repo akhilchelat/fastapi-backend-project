a9bbad87386e97efa22f0d2b10960459dd7f5371
Akhil Chelat <chelatakhilc@gmail.com>
2026-03-23 02:37:58 +0530

    database creation issue solved

--- app/main/main.py
+++ app/main/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
-
 from app.routes import user, employee,department, analytics
 
+from app.db.base import Base
+from app.db.session import engine
 
 app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
 
 app.include_router(user.router)
 app.include_router(employee.router)
